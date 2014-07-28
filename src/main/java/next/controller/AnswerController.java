2778f1328be99cbde76c1e85603a9ca2828a7a56
woonohyo <woonohyo@nhnnext.org>
2014-07-28 14:56:12 +0900

    AnswerController

---
+++ src/main/java/next/controller/AnswerController.java
@@ -0,0 +1,16 @@
+package next.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.mvc.Controller;
+
+public class AnswerController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		return null;
+	}
+
+}
